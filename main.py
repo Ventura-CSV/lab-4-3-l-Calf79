9453c45e38b8b38ce1c47c66eaab784a234ae41d
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-27 20:01:17 -0700

    Return in

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     numbers = [0] * 5
     for i in range(len(numbers)):
-        numbers[i] = int(input(f'Enter a value {i+1}:'))
+        numbers[i] = int(input('enter value'))
+        i+1
     total = 0
     for numbers in numbers:
         total += numbers   
